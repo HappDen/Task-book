a9fdadcf6b6ff475aedaa9bb8dd83083cb212d82
Denis Burlakov <53235180+HappDen@users.noreply.github.com>
2019-10-19 23:43:52 +0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,35 @@
+'''
+Task 1. Who are you?
+
+You flew to the planet of ultra developers. In order for you to be accepted in society, you need to introduce yourself correctly. For this you need to declare yourself. To do this, write your first official submission program, which will display your full name and surname, nickname, email, as well as age and native language.
+
+The conclusion should be made according to the template:
+
+Hi coders! I want to join your community!
+My name is [name] [surname]!
+My native language is [language]! And I'm proud of it!
+I was born [date of birth]. Not bad, right?
+You can contact me like [nickname]. I think we will make friends :) And I wish you a great time on our planet
+
+
+Input:
+Rick
+Sanchez           
+English           
+01.01.1949          
+Rick c-137
+
+
+Output:
+Hi coders! I want to join your community!
+My name is [Rick] [Sanchez]!
+My native language is [English]! And I'm proud of it!
+I was born [01.01.1949]. Not bad, right?
+You can contact me like [Rick c-137]. I think we will make friends :) And I wish you a great time on our planet.
+'''
+name = input()
+surname = input()
+language = input()
+age = input()
+nickname = input()
+print("Hi coders! I want to join your community! My name is" ,name, surname + "!" " My native language is " , language + "!" " And I'm proud of it! I was born", age + "." " Not bad, right? You can contact me like", nickname + "." " I think we will make friends :) And I wish you a great time on our planet.") 
